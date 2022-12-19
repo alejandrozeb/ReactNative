20a5edcbc0460cad068c12fb778d03c1169efd98
Alejandro Zeballos <alejandro.zeballos@fundacion-jala.org>
2022-12-19 19:09:56 -0400

    add input texts and a button example

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,12 @@
 import React from "react";
-import { TextInput, Button, Text } from "react-native";
+import { TextInput, Button, Text, View } from "react-native";
 
 export default function LoginForm() {
-    return <Text> Componente LoginForm</Text>;
+    return (
+        <View>
+            <TextInput placeholder="Email"/>
+            <TextInput placeholder="Contraseña"/>
+            <Button title="Enviar" onPress={() => console.log("Enviado")} />
+        </View>
+    );
 }
