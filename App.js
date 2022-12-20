7a7510be463df67bbbfeaf89395610dd526c58e4
Alejandro Zeballos <alejandro.zeballos@fundacion-jala.org>
2022-12-20 17:38:07 -0400

    add react navigation

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import { StyleSheet, Text, View } from "react-native";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
 import Saludar from "./src/components/Saludar";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>React Native</Text>
-      <Saludar name="Carlos Navarro" />
-      <Saludar name="Alejandro Z" />
-      <Saludar name="Ale Z" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>React Native</Text>
+        <Saludar firstname="Carlos" lastname="Navarro" />
+      </View>
+    </NavigationContainer>
   );
 }
 
--- src/components/Saludar.js
+++ src/components/Saludar.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { Text } from "react-native";
+import PropTypes from "prop-types";
 
 export default function Saludar(props) {
-    const { name } = props;
+    const { firstname, lastname } = props;
 
-    return <Text>Hola {name}</Text>
+    return <Text>Hola {firstname} {lastname} </Text>
+}
+
+Saludar.propTypes = {
+    firstname: PropTypes.string.isRequired, 
+    lastname: PropTypes.string,
 }
